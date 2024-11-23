88a0c15e121922857c7f4904c00010135e8b2d45
Deepak Gupta <dg8574144545@gmail.com>
2024-11-23 17:54:08 +0530

    code pushed

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,21 +3,23 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './Login.css';
 
+
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [error, setError] = useState('');
   const [isUserFound, setIsUserFound] = useState(null);
   const navigate = useNavigate();
 
+
+ const hoisted_api="https://skai-lama-backend.vercel.app";
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
+
     try {
       // Make API call to check if username exists
-      const response = await axios.post('
-                                         https://skai-lama-backend.vercel.app/api/login
-                                        // http://localhost:5000/api/login
-      ', { username });
+      const response = await axios.post(`${hoisted_api}/api/login`, { username });
        console.log("response",response);
       if (response.data.usernameExists) {
         setIsUserFound(true);
@@ -37,10 +39,7 @@ const LoginPage = () => {
 
     try {
       // Make API call to register a new user
-      const response = await axios.post('
-                                        https://skai-lama-backend.vercel.app/api/register
-                                        // http://localhost:5000/api/register
-      ', { username });
+      const response = await axios.post(`${hoisted_api}/api/register`, { username });
 
       if (response.status === 201) {
         alert('Registration successful! Please log in.');
